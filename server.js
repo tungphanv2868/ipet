240fc60ea630bec5fb8b2add9841ff6dc946f4e1
tungphanv2868 <91852251+tungphanv2868@users.noreply.github.com>
2023-06-09 01:31:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-// import path from "path";
+import path from "path";
+import { fileURLToPath } from 'url';
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
 
+//esmodeule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
 
@@ -21,7 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -30,13 +34,11 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
 
-// app.use("*", function(req, res) {
-//   res.sendFile(path.join(__dirname,"./client/build/index.html"));
-// })
+
+ app.use("*", function(req, res) {
+   res.sendFile(path.join(__dirname,"./client/build/index.html"));
+})
 
 
 //PORT
